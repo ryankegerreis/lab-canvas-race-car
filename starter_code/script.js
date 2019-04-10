2b27a90e3f0d59e641fbc7798e1cb9160473da16
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-04-10 12:39:28 -0400

    Board and car are positioned

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,60 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
-    startGame();
+    drawBoard();
+    drawCar();
   };
 
   function startGame() {
 
-  }
+  } 
 };
+
+
+
+function drawBoard() {
+  let canvas = document.querySelector('canvas');
+let w = 400;
+let h = 511;
+let ctx=canvas.getContext('2d');
+
+  frames++; 
+  ctx.beginPath();
+  ctx.fillStyle = "rgb(0, 110, 0)";
+  ctx.fillRect(0, 0, 400, h);
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.fillStyle = "gray";
+  ctx.fillRect(20, 0, 260, h);
+  ctx.closePath();
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(15, 0, 5, h);
+
+  ctx.beginPath(198, 0);
+  ctx.setLineDash([20, 20]);
+  ctx.lineWidth = "4"
+  ctx.strokeStyle = "white";
+  ctx.moveTo(150, 500);
+  ctx.lineTo(150, 0);
+  ctx.stroke();
+  
+  ctx.fillStyle = "white";
+  ctx.fillRect(280, 0, 5, h);
+}
+
+let carImage = new Image()
+carImage.src="./images/car.png"
+
+
+function drawCar(){
+  let canvas = document.querySelector('canvas');
+  let ctx=canvas.getContext('2d');
+  // let car = {
+  //   x:20,
+  //   y:20,
+  //   // accident: 40
+  // }
+  ctx.drawImage(carImage, 135, 120,25,25)
+}
+
