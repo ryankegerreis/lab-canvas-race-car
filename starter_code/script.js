d7f93b07eaab59f9989f3dfc2af6bc718e6943a2
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-04-10 18:17:23 -0400

    Mostly broken

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,17 +1,26 @@
+var myObstacles=[
+
+
+];
+
+//Startup Function
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+    this.interval = setInterval(updateCanvas, 200);
     drawBoard();
     drawCar();
     updateCanvas();
+    
   };
 
-  function startGame() {
+  // function startGame() {
 
   } 
-};
+// };
 
+let frames=0;
 
-
+//Game Board
 function drawBoard() {
 let canvas = document.querySelector('canvas');
 let w = 400;
@@ -44,6 +53,13 @@ let ctx=canvas.getContext('2d');
   ctx.fillRect(280, 0, 5, h);
 }
 
+//Car
+function drawCar(){
+  let canvas = document.querySelector('canvas');
+  let ctx=canvas.getContext('2d');
+  ctx.drawImage(carImage, car.x, car.y,25,25)
+}
+
 let carImage = new Image()
 carImage.src="./images/car.png"
 
@@ -52,21 +68,31 @@ let car = {
   y:120,
 }
 
-function drawCar(){
-  let canvas = document.querySelector('canvas');
-  let ctx=canvas.getContext('2d');
-  ctx.drawImage(carImage, car.x, car.y,25,25)
-}
-
+// Controls
 document.onkeydown = function(e) {
   console.log(e.keyCode)
   switch (e.keyCode) {
-    case 38: car.y-=5;  break;
-    case 40: car.y+=5;  break;
     case 37: car.x-=5;  break;
     case 39: car.x+=5;  break;
   }
 }
+
+//Obstacle Creator
+class Obstacle {
+  
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    // this.frames=frames;
+  }
+
+}
+
+// let f = 0
+
 
 function updateCanvas(){  
   let canvas = document.querySelector('canvas');
@@ -74,9 +100,46 @@ function updateCanvas(){
   let w = 400;
   let h = 511;
 
-  ctx.clearRect(0,0,w,h) 
+  // ctx.clearRect(0,0,w,h) 
   drawBoard()
   drawCar()
-
+  updateObstacles()
+  // console.log("It's updating")
   window.requestAnimationFrame(updateCanvas)
  }
+
+//Obstacles
+function updateObstacles() {
+//   let canvas = document.querySelector('canvas');
+//   let ctx=canvas.getContext('2d');
+//   // for (i = 0; i < myObstacles.length; i++) {
+//   //   myObstacles[i].x += -1;
+//   //   myObstacles[i].updateCanvas();
+//   // }
+//   // console.log("obstacles function has been called")
+  
+//   frames += 1;
+
+//   if (frames % 120 === 0) {
+//     console.log("the conditional happened")
+//     var x = drawBoard.w;
+//     var minHeight = 20;
+//     var maxHeight = 100;
+//     var height = Math.floor(
+//       Math.random() * (maxHeight - minHeight + 1) + minHeight
+//     );
+//     var minGap = 50;
+//     var maxGap = 200;
+//     var gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+//     myObstacles.push(new Obstacle(10, height, "red", 20, 0));
+//     console.log(myObstacles)
+//       // ctx.fillStyle='blue'
+//       // ctx.fillRect(150,0,100,100)
+
+//   }
+
+//   for (i = 0; i < myObstacles.length; i++) {
+//     myObstacles[i].x += -1;
+//     // updateCanvas();
+//   }
+}
